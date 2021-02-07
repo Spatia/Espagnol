36ac5ee7c28ad9ada5b3741ce3d633bafecb837c
Spatia <50461472+Spatia@users.noreply.github.com>
2021-02-07 19:33:55 +0100

    Add files via upload

---
+++ Espa.py
@@ -0,0 +1,17 @@
+from random import *
+import os
+
+verbes = open("verbes.txt","r")
+temps = open("temps.txt","r")
+r = verbes.readlines()
+tps = temps.readlines()
+
+nbv = int(input("Nombre de verbes: "))
+os.system("cls")
+for i in range(nbv):
+    pick = r.pop(randint(0,len(r)-1))
+    pick = pick[0:len(pick)-1]
+    tpps = tps[randint(0,len(tps)-1)]
+    print(str(pick) + " " + str(tpps),end="")
+
+rd = randint(0,100)
